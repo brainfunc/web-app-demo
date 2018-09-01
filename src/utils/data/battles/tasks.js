d08a3ac07f39a7970172c5bc66a7810412f755f0
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-09-01 17:03:18 +0530

    Task correct ans

--- src/utils/data/battles/tasks.js
+++ src/utils/data/battles/tasks.js
@@ -7,21 +7,21 @@ export const Data={
       out on your way where you notice many white Jasmine flowers lying on the garden
       bench. You smell some of them. Which brain parts make you enjoy the
       refreshing fragrance?`,
-      correctCombination: [1,2,3,4]
+      correctCombination: [2,5,7]
     },{
       image: `/style/images/battles/tasks/fight.png`,
       title: `The Sword Fight!`,
       description: `Your friend just challenged you to a fist fight. You said why
       not use toy swords instead? You are confident in your capability of balance
       and attack. What parts do you use? Select all that apply.`,
-      correctCombination: [1,2,3,4]
+      correctCombination: [2,3,5,6,8,9,10,11,12]
     },{
       image: `/style/images/battles/tasks/music.png`,
       title: `Listening to Eminem's Rap God`,
       description: `Your favorite artist eminem just dropped a beat and is currently
       trending #1 on youtube. You decide to turn on spotify, close your eyes and
       immerse yourself in the enchanting music. What parts do you use?`,
-      correctCombination: [1,2,3,4]
+      correctCombination: [3,4,8]
     },{
       image: `/style/images/battles/tasks/video.png`,
       title: `Watching Batman conquer terror in Gotham`,
@@ -29,7 +29,7 @@ export const Data={
       to theaters. Your friend generously offered you a ticket to the front row
       seats for the first show of the evening. Which brain parts will primarily be
       used while watching the awesome movie?`,
-      correctCombination: [1,2,3,4]
+      correctCombination: [1,2,3,4,5,8]
     },{
       image: `/style/images/battles/tasks/song.png`,
       title: `Rock that Karaoke!`,
@@ -37,7 +37,26 @@ export const Data={
       decide to chill out with your friends. Plans are ripe for evening out. Everybody
       decides to go to Karaoke and sing their hearts out. You are the kind of Karaoke
       in your group. What parts will you use while singing?`,
-      correctCombination: [1,2,3,4]
+      correctCombination: [3,4,8,10]
+    },
+    {
+      image: `/style/images/battles/tasks/math.png`,
+      title: `Math problems...`,
+      description: `Seems like math is your favorite subject. You need to solve a
+      particularly hard problem, which your teacher has given you as homework. You
+      don't have to write anything but figure out a way to approach the problem. Which
+      of your brainy parts will help you the most?`,
+      correctCombination: [0,1,2,5,11]
+    },
+    {
+      image: `/style/images/battles/tasks/book.png`,
+      title: `The Game of Thrones!`,
+      description: `You've thorogouhly enjoyed the show and now have decided to
+      immerse yourself in the vastness of the book - Game of thrones. You are
+      spending your lazy Sunday afternoon reading Game of Thrones - A Clash of
+      Clans. Which parts are necessary to imagine and have a wonderful experience
+      reading the book?`,
+      correctCombination: [1,2,5,11]
     }
   ]
 }
