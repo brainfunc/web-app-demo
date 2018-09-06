60f13f85696424f07b8c24250fe93a62a2617211
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-09-06 08:41:01 +0530

    Correct ans to tasks

--- src/utils/data/battles/tasks.js
+++ src/utils/data/battles/tasks.js
@@ -1,3 +1,189 @@
+// Brainparts:[
+//   {
+//     category:"Cerebrum",
+//     subcategory:"Frontal Lobe(Left)",
+//     categoryIndex:0,
+//     subcategoryIndex:0,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/leftfrontal.png",
+//     description: `Performs core cognition function - Most important part, that
+//     controls important cognitive skills in humans, such as emotional expression,
+//     problem solving, memory, language, judgment, and sexual behaviors. The left part
+//     is associated with logical cognition.`
+//   },
+//   {
+//     category:"Cerebrum",
+//     subcategory:"Frontal Lobe(Right)",
+//     categoryIndex:0,
+//     subcategoryIndex:1,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/rightfrontal.png",
+//     description: `Performs core cognition function - Most important part, that
+//     controls important cognitive skills in humans, such as emotional expression,
+//     problem solving, memory, language, judgment, and sexual behaviors. The right part
+//     is associated with creative abilities.`
+//   },
+//   {
+//     category:"Cerebrum",
+//     subcategory:"Pareital Lobe",
+//     categoryIndex:0,
+//     subcategoryIndex:2,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/pareital.png",
+//     description: `Involved in time keeping - The parietal lobes can be divided
+//     into two functional regions. One involves sensation and perception and the
+//     other is concerned with integrating sensory inputm. The first function
+//     integrates sensory information to form a single perception (cognition).
+//     This makes it involved in tasks sucj as perception of time, sensation
+//     and perception.`
+//   },
+//   {
+//     category:"Cerebrum",
+//     subcategory:"Temporal Lobe",
+//     categoryIndex:0,
+//     subcategoryIndex:3,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/temporal.png",
+//     description: `Auditory / Hearing functions - The temporal lobe is involved
+//     in primary auditory perception, such as hearing, and holds the primary auditory
+//     cortex. The primary auditory cortex receives sensory information from the
+//     ears and secondary areas process the information into meaningful units such
+//     as speech and words.`
+//   },
+//   {
+//     category:"Cerebrum",
+//     subcategory:"Occipital Lobe",
+//     categoryIndex:0,
+//     subcategoryIndex:4,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/occipital.png",
+//     description: `Visual processing function - The occipital lobe is one of the
+//     four major lobes of the cerebral cortex in the brain of mammals. The occipital
+//     lobe is the visual processing center of the mammalian brain containing most
+//     of the anatomical region of the visual cortex.`
+//   },
+//   {
+//     category:"Cerebellum",
+//     subcategory:"Anterior Lobe",
+//     categoryIndex:1,
+//     subcategoryIndex:5,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/anteriorlobe.png",
+//     description: `Regulates posture - The hemispheres are separated from one
+//     another by a thin structure called the vermis, or "worm." The anterior lobe,
+//     or paleocerebellum, is the second oldest part of the cerebellum. It receives
+//     proprioceptive input from the spinal cord and controls the anti-gravity
+//     muscles of the body, thus regulating posture.`
+//   },
+//   {
+//     category:"Cerebellum",
+//     subcategory:"Posterior Lobe",
+//     categoryIndex:1,
+//     subcategoryIndex:6,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/posteriorlobe.png",
+//     description: `Involved in Kidney and Urinary function - Two hormones are
+//     classically considered as being related to the posterior pituitary: oxytocin
+//     and vasopressin. These hormones are created in the hypothalamus and released
+//     in the posterior pituitary. Bad functioning causes frequent urination and
+//     kidney issues.`
+//   },
+//   {
+//     category:"Cerebellum",
+//     subcategory:"Flocculonodular Lobe",
+//     categoryIndex:1,
+//     subcategoryIndex:7,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/flocculonodularlobe.png",
+//     description: `Involved in visual processing - The flocculus is a part of
+//     the vestibuloocular reflex system and is used to help stabilize gaze during
+//     head rotation about any axis of space. Neurons in both the vermis of cerebellum
+//     and flocculus transmit an eye velocity signal that correlates with smooth pursuit.`
+//   },
+//   {
+//     category:"Brainstem",
+//     subcategory:"Midbrain",
+//     categoryIndex:2,
+//     subcategoryIndex:8,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/midbrain.png",
+//     description: `Involved in Auditory Processing - Midbrain, also called mesencephalon,
+//     region of the developing vertebrate brain that is composed of the tectum and
+//     tegmentum. The midbrain serves important functions in motor movement, particularly
+//     movements of the eye, and in auditory processing (also visual processing).`
+//   },
+//   {
+//     category:"Brainstem",
+//     subcategory:"Pons",
+//     categoryIndex:2,
+//     subcategoryIndex:9,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/pons.png",
+//     description: `Muscle communication, balance co-ordinator - Besides the medulla oblongata,
+//     your brainstem also has a structure called the pons. The pons is a major structure
+//     in the upper part of your brainstem. It is involved in the control of breathing,
+//     communication between different parts of the brain, and sensations such as balance.`
+//   },
+//   {
+//     category:"Brainstem",
+//     subcategory:"Medulla",
+//     categoryIndex:2,
+//     subcategoryIndex:10,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/medulla.png",
+//     description: `Info carrier for body - The medulla oblongata helps regulate breathing,
+//     heart and blood vessel function, digestion, sneezing, and swalLow Strengthing.
+//     This part of the brain is a center for respiration and circulation. Sensory and motor
+//     neurons (nerve cells) from the forebrain and midbrain travel through the medulla.`
+//   },
+//   {
+//     category:"Arterial",
+//     subcategory:"Basilar Artery",
+//     categoryIndex:3,
+//     subcategoryIndex:11,
+//     strength:"0",
+//     neuronQuantity: "3",
+//     image: "/style/images/collectibles/brainparts/basilarartery.png",
+//     description: `Cognition based blood supply. - The basilar artery carries oxygenated
+//     blood to the cerebellum, brainstem, and occipital lobes. The cerebellum helps with
+//     voluntary processes such as posture, balance, coordination, and speech.`
+//   },
+//   {
+//     category:"Arterial",
+//     subcategory:"Vertebral Artery",
+//     categoryIndex:3,
+//     subcategoryIndex:12,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/vertebralartery.png",
+//     description: `Dexterity based blood supply - Each vessel courses superiorly along
+//     each side of the neck, merging within the skull to form the single, midline basilar
+//     artery. As the supplying component of the vertebrobasilar vascular system, the
+//     vertebral arteries provide supply blood to the upper spinal cord, brainstem,
+//     cerebellum, and posterior part of brain.`
+//   },
+//   {
+//     category:"Unknown",
+//     subcategory:"uPart (Soul)",
+//     categoryIndex:4,
+//     subcategoryIndex:13,
+//     strength:"0",
+//     neuronQuantity: "0",
+//     image: "/style/images/collectibles/brainparts/soulpart.png",
+//     description: ``
+//   }
+// ],
 export const Data={
   tasks: [
     {
@@ -7,21 +193,21 @@ export const Data={
       out on your way where you notice many white Jasmine flowers lying on the garden
       bench. You smell some of them. Which brain parts make you enjoy the
       refreshing fragrance?`,
-      correctCombination: [2,5,7]
+      correctCombination: [0,1,9,10]
     },{
       image: `/style/images/battles/tasks/fight.png`,
       title: `The Sword Fight!`,
       description: `Your friend just challenged you to a fist fight. You said why
       not use toy swords instead? You are confident in your capability of balance
       and attack. What parts do you use? Select all that apply.`,
-      correctCombination: [2,3,5,6,8,9,10,11,12]
+      correctCombination: [0,2,4,5,7,9,10,11,12]
     },{
       image: `/style/images/battles/tasks/music.png`,
       title: `Listening to Eminem's Rap God`,
       description: `Your favorite artist eminem just dropped a beat and is currently
       trending #1 on youtube. You decide to turn on spotify, close your eyes and
       immerse yourself in the enchanting music. What parts do you use?`,
-      correctCombination: [3,4,8]
+      correctCombination: [1,2,3,8]
     },{
       image: `/style/images/battles/tasks/video.png`,
       title: `Watching Batman conquer terror in Gotham`,
@@ -29,7 +215,7 @@ export const Data={
       to theaters. Your friend generously offered you a ticket to the front row
       seats for the first show of the evening. Which brain parts will primarily be
       used while watching the awesome movie?`,
-      correctCombination: [1,2,3,4,5,8]
+      correctCombination: [1,2,3,4,7,8]
     },{
       image: `/style/images/battles/tasks/song.png`,
       title: `Rock that Karaoke!`,
@@ -37,7 +223,7 @@ export const Data={
       decide to chill out with your friends. Plans are ripe for evening out. Everybody
       decides to go to Karaoke and sing their hearts out. You are the kind of Karaoke
       in your group. What parts will you use while singing?`,
-      correctCombination: [3,4,8,10]
+      correctCombination: [2,3,8,9]
     },
     {
       image: `/style/images/battles/tasks/math.png`,
@@ -46,7 +232,7 @@ export const Data={
       particularly hard problem, which your teacher has given you as homework. You
       don't have to write anything but figure out a way to approach the problem. Which
       of your brainy parts will help you the most?`,
-      correctCombination: [0,1,2,5,11]
+      correctCombination: [0,1,4,7,11]
     },
     {
       image: `/style/images/battles/tasks/book.png`,
@@ -56,7 +242,7 @@ export const Data={
       spending your lazy Sunday afternoon reading Game of Thrones - A Clash of
       Clans. Which parts are necessary to imagine and have a wonderful experience
       reading the book?`,
-      correctCombination: [1,2,5,11]
+      correctCombination: [0,1,4,7,11]
     }
   ]
 }
